30651f2c3ca416013682280ffd836b2593331ab0
Maria Gabriella Brodi <mbrodi@pivotal.io>
2020-08-19 23:25:34 -0400

    kustomized config map for kmz

--- switch-accounts.sh
+++ switch-accounts.sh
@@ -15,7 +15,7 @@ fi
 find ./yaml -type f \( -name "*.yaml" -o -name "*.yml" \) -print0 | xargs -0 sed -i '' "s/${FROM_REG}/${TO_REG}/g"
 find ./yaml -type f \( -name "*.yaml" -o -name "*.yml" \) -print0 | xargs -0 sed -i '' "s/${FROM_DMN}/${TO_DMN}/g"
 
-sed -i '' 's/marygabry1508/ciberkleid/g' docker-push.sh
+sed -i '' "s/${FROM_REG}/${TO_REG}/g" docker-push.sh
 
 
 
